8279a95ff9bd4746163a7ef17862ae74ab551980
ezequiasdanel <ezequiasdanel@hotmail.com>
2021-11-03 19:07:53 -0300

    primer commit

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import './index.css';
+
+function Header() {
+  return (
+    <Fragment>
+      <header className="header" >
+      <a herf="" className="header-logo">Control+Alt+Suprimir Informatica</a>
+        <nav className="header-nav">
+          <a herf=" "className="header-link">Home</a>
+          <a herf=" "className="header-link">Componentes</a>
+          <a herf=" "className="header-link">Quienes Somos</a>
+          <a herf=" "className="header-link">Soporte</a>
+        </nav>
+      </header>
+    </Fragment>
+  );
+}
+
+export default Header;
