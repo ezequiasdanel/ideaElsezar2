422c8f1940921d86839cc490a04b8368d28d4c59
ezequiasdanel <ezequiasdanel@hotmail.com>
2021-11-15 21:12:09 -0300

    tercercommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { Fragment } from "react";
 import Header from "./componentes/Header/Header"
 import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
-<<<<<<< HEAD
-=======
 import ItemCount from "./componentes/ItemCount/ItemCount";
->>>>>>> 1cbe614 (commit2)
+
 function App () {
     return(
         <Fragment>
         <Header />
-<<<<<<< HEAD
-        <ItemListContainer greeting="Bienvenidos a Control Alt Suprimir Informatica"/>
-=======
         <ItemListContainer/>
-            <ItemCount/>
->>>>>>> 1cbe614 (commit2)
+        <ItemCount/>
         </Fragment>
     )
 }
